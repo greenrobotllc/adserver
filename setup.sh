b73788498628794f8eb161816e8e382cc15509a9
Andy Triboletti <andy.triboletti@gmail.com>
2018-12-29 16:53:39 -0500

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+php composer.phar install --no-scripts;
+mkdir bootstrap/cache;
+php composer.phar install;
+php artisan migrate;
+php artisan db:seed;
+php artisan key:generate;
+php artisan config:cache;
+php artisan config:clear;
+git submodule update --init --recursive
+cd laradock; cp env-example .env
+docker-compose down
+docker-compose up -d nginx mysql
